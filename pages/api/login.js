8ee379272811303452a278d50e4f1eb98a16dd30
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-10-31 19:38:59 -0400

    fixed server response

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,6 +5,6 @@ export default async function login(req, res) {
   const magic = new Magic(process.env.MAGIC_SECRET_KEY);
   const didToken = req.headers.authorization.substr(7);
   const metaData = await magic.users.getMetadataByToken(didToken);
-  const token = await setLoginSession(res, metaData);
-  res.send({ done: true, token: token });
+  await setLoginSession(res, metaData);
+  res.send({ done: true });
 }
