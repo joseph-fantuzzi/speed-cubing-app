e82a626217f001d078267cb646b0dd50a7f39dd8
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-10-31 20:49:00 -0400

    added cube route with a 3d cube

---
+++ pages/cube.js
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Cube() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Spline scene="https://prod.spline.design/UpO17lyuZjsWesSU/scene.splinecode" />
+    </div>
+  );
+}
