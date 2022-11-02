2274fe9fc98eab917fecd513c579d59e0e9ff87c
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-11-02 18:48:32 -0400

    changed 3d spline model and updated css files

---
+++ pages/components/cube.js
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Cube() {
+  return (
+    <div style={{ height: "600px", width: "75%", margin: "0 auto" }}>
+      <Spline scene="https://prod.spline.design/7NVPfOQiG2WPECyD/scene.splinecode" />
+    </div>
+  );
+}
--- pages/cube.js
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-export default function Cube() {
-  return (
-    <div style={{ height: "100vh" }}>
-      <Spline scene="https://prod.spline.design/UpO17lyuZjsWesSU/scene.splinecode" />
-    </div>
-  );
-}
